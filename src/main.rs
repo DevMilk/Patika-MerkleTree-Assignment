c79b2920e883ac96192007ac25da0e4b11ef17f4
Uğur <ugurkk2599@gmail.com>
2022-09-19 14:53:32 +0300

    Updated fork

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use sha2::{Sha256, Digest};
-
-fn main(filename: String) -> String {
+fn main(){
+    
+}
+fn merkle_root(filename: String) -> String {
     // Read Input Data from txt file
     let file = File::open(filename).expect("Could not open file");
     let reader = BufReader::new(file);
@@ -101,31 +103,36 @@ mod tests {
     use super::*;
     #[test]
     fn input_0() {
-        let result = main("input0.txt".to_string());
+        let result = merkle_root
+    ("input0.txt".to_string());
         assert_eq!(result, "ff41418be11ed77612aeb83ee0bcf97a5853a4c291e23bd4d4cc6435fcfabdf9");
     }
 
     #[test]
     fn input_1() {
-        let result = main("input1.txt".to_string());
+        let result = merkle_root
+    ("input1.txt".to_string());
         assert_eq!(result, "98a77b2c3ff5f6c2aca697f60b2aa2a1a2733be36dbd35bae23d517c2ad5985e");
     }
 
     #[test]
     fn input_2() {
-        let result = main("input2.txt".to_string());
+        let result = merkle_root
+    ("input2.txt".to_string());
         assert_eq!(result, "3c0fb0638de91551eae4e9d984d72034aa0693be37b51737e6b81bc489866e5e");
     }
 
     #[test]
     fn input_3() {
-        let result = main("input3.txt".to_string());
+        let result = merkle_root
+    ("input3.txt".to_string());
         assert_eq!(result, "f03b1c9163babeb728ac011fe0c2c9c69700a2f8ddde211ec07d621cdb322cfe");
     }
 
     #[test]
     fn input_4() {
-        let result = main("input4.txt".to_string());
+        let result = merkle_root
+    ("input4.txt".to_string());
         assert_eq!(result, "f83e74742fda659dfc07615881af796abafc434f591aeb23b9f4366abe03c597");
     }
 }
